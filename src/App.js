357c8f2558676b3e589a1f15134cd2f8064f62a7
FoboIX <mills_forrest@yahoo.com>
2023-03-11 13:55:17 -0600

    added styling todo component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ const style = {
   bg: `h-screen w-screen p-4`,
   container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl, p-4`,
   heading: `text-3xl font-bold, text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-3xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-2`,
 };
 
 function App() {
@@ -15,9 +19,13 @@ function App() {
     <div className={style.bg}>
       <div className={style.container}>
         <h3 className={style.heading}>Action Items</h3>
-        <form className="form">
-          <input type="text" placeholder="Add Habit"></input>
-          <button className="button">
+        <form className={style.form}>
+          <input
+            className={style.input}
+            type="text"
+            placeholder="Add Habit"
+          ></input>
+          <button className={style.button}>
             <IoMdAddCircle size={30} />
           </button>
         </form>
